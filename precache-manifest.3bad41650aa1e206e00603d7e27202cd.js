ec0c88bca5ef78438e1a22a01fd4e739fddbc0a9
RomanSamohwal <rsamohwal@gmail.com>
2020-09-18 03:33:07 +0300

    Updates

---
+++ precache-manifest.3bad41650aa1e206e00603d7e27202cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c76ac0c13af4cb53eee23bc2459bf739",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "df8c2bd0338875e60503",
+    "url": "/portfolio/static/css/main.c8abf02f.chunk.css"
+  },
+  {
+    "revision": "8fb51567ed9ca69e203e",
+    "url": "/portfolio/static/js/2.5e3d1481.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.5e3d1481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df8c2bd0338875e60503",
+    "url": "/portfolio/static/js/main.428ac00e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "4b2162ae8fdb6fa1b63af905d8960289",
+    "url": "/portfolio/static/media/photo_2020-05-20_00-50-23.4b2162ae.jpg"
+  },
+  {
+    "revision": "f0b38d4bb327b405d43c9050147a6a53",
+    "url": "/portfolio/static/media/sn3.f0b38d4b.jpg"
+  },
+  {
+    "revision": "ad11a56be49f66edfd2603608cab253c",
+    "url": "/portfolio/static/media/todolist3.ad11a56b.jpg"
+  }
+]);
